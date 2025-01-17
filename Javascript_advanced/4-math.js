2d4d9163734459e95e765ab3505bfbcf1c772e19
niniisg <nisselsanchez@gmail.com>
2025-01-17 16:31:03 -0400

    complex closure

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -10,7 +10,7 @@ function addBy(firstNumber) {
     };
 }
 
-console.log(addBy100(20));
-console.log(divideBy10(20));
-console.log(divideBy100(200));
-console.log(addBy1000(20));
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
