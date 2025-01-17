016b1bbbb8b480883126bba93457028b74911ffb
niniisg <nisselsanchez@gmail.com>
2025-01-16 22:45:29 -0400

    fixing typo

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -4,6 +4,6 @@ function welcomeMessage(fullName){
 
 }
 
-const guillame = welcomeMessage("Guillame");
+const guillame = welcomeMessage("Guillaume");
 const alex = welcomeMessage("Alex");
 const fred = welcomeMessage("Fred");
