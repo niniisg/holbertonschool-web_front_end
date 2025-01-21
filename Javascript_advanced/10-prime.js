ca22687e13ff79ce97c25e153c4e2fd09d4d0ce8
niniisg <nisselsanchez@gmail.com>
2025-01-21 17:45:36 -0400

    execution stack

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let num = 2; num <= 100; num++) {
+        let primeNum = true;
+
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                primeNum = false;
+                break;
+            }
+        }
+        
+        if (primeNum) {
+            count++;
+        }
+    }
+    return count;
+}
+
+const start = performance.now();
+for (let i = 0; i < 10; i++) {
+    countPrimeNumbers();
+    
+}
+const end = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${end - start} milliseconds.`);
